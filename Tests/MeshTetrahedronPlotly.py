b7a948b9e96ebbb3b9577c179c2599d1005d423a
Gaspard <47181975+GaspardLauras@users.noreply.github.com>
2020-05-31 13:57:05 +0200

    yo2

---
+++ Tests/MeshTetrahedronPlotly.py
@@ -0,0 +1,47 @@
+import plotly.graph_objects as go
+
+fig = go.Figure(data=[
+    go.Figure(
+        x=[0, 1, 2, 0],
+        y=[0, 0, 1, 2],
+        z=[0, 2, 0, 1],
+        colorbar_title='z',
+        colorscale=[[0, 'gold'],
+                    [0.5, 'mediumturquoise'],
+                    [1, 'magenta']],
+        # Intensity of each vertex, which will be interpolated and color-coded
+        intensity=[0, 0.33, 0.66, 1],
+        # i, j and k give the vertices of triangles
+        # here we represent the 4 triangles of the tetrahedron surface
+        i=[0, 0, 0, 1],
+        j=[1, 2, 3, 2],
+        k=[2, 3, 1, 3],
+        name='y',
+        showscale=True
+    )
+])
+
+
+#fig.show()
+
+fig2 = go.Figure(data=[
+    go.Mesh3d(
+        x=[0, 1, 2, 0],
+        y=[0, 0, 1, 2],
+        z=[0, 2, 0, 1],
+        colorbar_title='z',
+        colorscale=[[0, 'gold'],
+                    [0.5, 'mediumturquoise'],
+                    [1, 'magenta']],
+        # Intensity of each vertex, which will be interpolated and color-coded
+        intensity=[0, 0.33, 0.66, 1],
+        # i, j and k give the vertices of triangles
+        # here we represent the 4 triangles of the tetrahedron surface
+        i=[0, 1, 2, 3],
+        j=[0, 1, 2, 3],
+        k=[0, 1, 2, 3],
+        name='y',
+        showscale=True
+    )
+])
+fig2.show()
