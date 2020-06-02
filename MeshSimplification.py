673d2833fde00d15e938e8d02085017c2c588daf
Gaspard <47181975+GaspardLauras@users.noreply.github.com>
2020-06-02 22:38:50 +0200

    Ajout de la fonction planEquation()
    Fonction qui renvoie les parametre a,b,c,d de l'équation du plan défini par les trois points en paramètre sous la forme ax+by+cz+d=0 sous la forme du vecteur p=[a b c d] et de sa transposée pt. 
    P1,P2,P3 vecteurs numpy np.array([x,y,z]) représentant les coordonnées des trois points délimitant la surface et donc le plan.

--- MeshSimplification.py
+++ MeshSimplification.py
@@ -1,12 +1,46 @@
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 from MeshPlot import plotMesh
+import numpy as np
 import meshio
 
-offName = "OFF/tri_gargoyle.off"
+def planEquation(threePointsCoords): #
+    """
+    Fonction qui renvoie l'équation du plan défini par les trois points en 
+    paramètre sous la forme ax+by+cz+d=0
+    P1,P2,P3 vecteurs numpy np.array([x,y,z]) représentant les coordonnées des
+    trois points délimitant la surface et donc le plan.
+    """
+    p1,p2,p3 = threePointsCoords
+    v1 = p2-p1
+    v2 = p3-p1 
+    
+    vn = np.cross(v1,v2)
+    a,b,c = vn
+    one = a**2+b**2+c**2
+    vn = vn/np.sqrt(one)
+    d = np.dot(vn, p3)
+    #print('{0}x+{1}y+{2}z+{3}'.format(a,b,c,d))
 
+    p = np.array([a,b,c,d])
+    pt = p[np.newaxis].T 
+    """ print(p)
+    print(pt)
+    print('Kp : \n',p*pt) """
+    return p,pt
+
+
+
+
+offName = "OFF/tri_gargoyle.off"
 #Extraction des données
 mesh = meshio.read(filename=offName,file_format="off")
 sommets = mesh.points
 faces = mesh.cells[0].data
 
-plotMesh(sommets,faces,offName)
+planEquation(sommets[faces[0]])
+
+#plotMesh(sommets,faces,offName)
+
+
 
