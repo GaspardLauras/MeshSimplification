4928b3435da0a3f737b10e0bb4c170edec0c86a2
Gaspard <47181975+GaspardLauras@users.noreply.github.com>
2020-06-15 18:16:55 +0200

    Avancement sur v

--- MeshSimplification.py
+++ MeshSimplification.py
@@ -7,7 +7,7 @@ from SimplificationFonctions import *
 from Sommet import Sommet
 
 def init(sommets,faces):
-    validPairsIndex = get_validPairs(sommets,faces)
+    #Calcul de Q pour tous les sommets initiaux
     points_in_surface = get_Points_in_surface(sommets,faces)
     Kps = get_Kps(faces,points_in_surface)
     Q = get_Q(Kps)
@@ -18,10 +18,13 @@ def init(sommets,faces):
         sommetsCLass[-1].set_Kp(Kps[i])
         sommetsCLass[-1].set_Q(Q[i])
         sommetsCLass[-1].set_surfaces(points_in_surface[i])
+    
+    #Selection des paires valides
+    validPairsIndex = get_validPairs(sommets,faces)
+
     return validPairsIndex,sommetsCLass
 
 offName = "OFF/test.off"
-
 
 #########################################
 #       Extraction des données:         #
@@ -29,30 +32,33 @@ offName = "OFF/test.off"
 mesh = meshio.read(filename=offName,file_format="off")
 sommets = mesh.points
 faces = mesh.cells[0].data
-#plotMesh(sommets,faces,offName)
-plotScatter(sommets,offName)
+plotMesh(sommets,faces,offName)
+#plotScatter(sommets,offName)
 
 validPairsIndex, sommets = init(sommets,faces)
 #print(sommets)
+print(validPairsIndex)
+print('---------------------------------')
 
 newSommet = []
 for pair in validPairsIndex:
     Q = sommets[pair[0]].Q + sommets[pair[1]].Q 
-    Qp = [[Q[0][0],Q[0][1],Q[0][2],Q[0][3]],
-          [Q[0][1],Q[1][1],Q[1][2],Q[1][3]],
-          [Q[0][2],Q[1][2],Q[2][2],Q[2][3]],
+    Qp = [[Q[0][0],Q[1][0],Q[2][0],Q[3][0]],
+          [Q[0][1],Q[1][1],Q[2][1],Q[3][1]],
+          [Q[0][2],Q[1][2],Q[2][2],Q[3][2]],
           [   0   ,   0   ,   0   ,   1   ]]
     Qp = np.array(Qp)
-    #print('Qp : \n',Qp)
-    #print('Det : \n',np.linalg.det(Qp))
+    print('Qp : \n',Qp)
+    print('Det : \n',np.linalg.det(Qp))
     Qp = np.linalg.inv(Qp)
-    #print('Qp-1 : \n',Qp)
+    print('Qp^-1 : \n',Qp)
 
     #print('----')
-    v = Qp @ np.array([[0],[0],[0],[1]])
+    v = Qp.dot(np.array([[0],[0],[0],[1]]))
     #print('new V : \n',v)
     newSommet.append([v[0],v[1],v[2]])
-    #print('----------')
+    print('----------')
 newSommet = np.array(newSommet)
 print(newSommet)
-plotScatter(newSommet,offName)
+print(len(newSommet))
+#plotScatter(newSommet,offName)
