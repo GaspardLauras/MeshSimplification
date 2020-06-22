56235bfbddf9c2eba035a6a4a114799b1d6d0732
Gaspard <47181975+GaspardLauras@users.noreply.github.com>
2020-06-22 21:18:20 +0200

    Début du bouclage de contraction

--- MeshSimplification.py
+++ MeshSimplification.py
@@ -16,76 +16,16 @@ offName = "OFF/test.off"
 mesh = meshio.read(filename=offName,file_format="off")
 sommets = mesh.points
 faces = mesh.cells[0].data
-plotMesh(sommets,faces,'AVANT')
-sommetsCoords = sommets
+#plotMesh(sommets,faces,'AVANT')
+
 
 
 
 ################################
-validPairsIndex, sommets = init(sommets,faces)
-
-#print('Valid pairs : \n',validPairsIndex)
-"""
-A TROUVER : QUAND EST-CE QU'ON S'ARRETE?? --> il faut donner un nombre de faces à obtenir à l'avance
-"""
-
-contractedSommets = []
-newDv = []
-newDic = []
-for pair in validPairsIndex:
-    #print('Pair : ',pair)
-    print('-------------')
-    Q = sommets[pair[0]].Q + sommets[pair[1]].Q 
-    print('Q : \n',Q)
-    Qp = [[Q[0][0],Q[0][1],Q[0][2],Q[0][3]],
-		  [Q[0][1],Q[1][1],Q[1][2],Q[1][3]],
-		  [Q[0][2],Q[1][2],Q[2][2],Q[2][3]],
-		  [0      ,0      ,0      ,1      ]]
-    Qp = np.array(Qp)
-    print('Qp : \n',Qp)
-    print('Det : \n',np.linalg.det(Qp))
-
-    if np.linalg.det(Qp) > 10**(-4):
-        Qp = np.linalg.inv(Qp)
-        v = Qp.dot(np.array([[0],[0],[0],[1]]))
-    else:
-        v= (sommets[pair[0]].coords + sommets[pair[1]].coords)/2
-    
-    contractedSommets.append(v[0:3])
-    #print('new V : \n',v)
-    Dv = cost(v,Q)
-    #print('Dv : \n',Dv)
-    newDv.append(Dv)
-    newDic.append((Dv,v,pair))
-    #print('----------')
-
-
-
-
-#print('--------------------------------')
-contractedSommets = np.array(contractedSommets)
-newDv = np.array(newDv)
-
-newDic = sorted(newDic) #Trie selon le premier élément du tuple
-newDic = np.array(newDic)
-#print('New dic : \n',newDic)
-
-minCost, minV, minPair = newDic[0]
-
-
-newSommets = copy.deepcopy(sommetsCoords)
-
-print('sommets not updated : \n',newSommets)
-newSommets[minPair[0]] = np.transpose(minV[0:3])
-newSommets[minPair[1]] = np.transpose(minV[0:3])
-#_,idx = np.unique(newSommets, axis=0, return_index=True)
-#idx = np.sort(idx)
-#newSommets = newSommets[idx]
-print('sommets updated without doublons : \n',newSommets)
-#print('Faces : \n',faces)
-
-
-#plotScatterMatplot(sommetsCoords)
-#plot2ScatterMatplot(sommetsCoords,newSommets)
-plotMesh(newSommets,faces,'APRES')
+for i in range(20):
+    sommets, faces = contraction(sommets, faces)
 ################
+
+
+#plot2ScatterMatplot(sommetsCoords,newSommets)
+plotMesh(sommets,faces,'APRES')
--- SimplificationFonctions.py
+++ SimplificationFonctions.py
@@ -1,32 +1,8 @@
 import numpy as np
 from Sommet import Sommet
 import meshio
-
-
-def init(sommets,faces):
-    #Calcul de Q pour tous les sommets initiaux
-    points_in_surface = get_Points_in_surface(sommets,faces)
-    Kps = get_Kps(sommets,points_in_surface)
-    #print('Kps : \n',Kps)
-    #Q = get_Q(Kps)
-    #deltaVs = get_deltaVs(sommets,Q)
-    #print(Q)
-    sommetsCLass = []
-    for i in range(len(sommets)):
-        sommetsCLass.append(Sommet(sommets[i]))
-        sommetsCLass[-1].set_Kp(Kps[i])
-        sommetsCLass[-1].set_Q()
-        sommetsCLass[-1].set_surfaces(points_in_surface[i])
-        g = sommetsCLass[-1]
-        c = np.concatenate((g.coords, np.array([1])),axis=0)
-        #print(c)
-        #print('cost : ',c@g.Q@c.transpose())
-    
-    #Selection des paires valides
-    validPairsIndex = get_validPairs(sommets,faces)
-
-    return validPairsIndex,sommetsCLass
-
+import copy
+from MeshPlot import *
 
 
 def planEquation(threePointsCoords):
@@ -37,11 +13,11 @@ def planEquation(threePointsCoords):
     trois points délimitant la surface et donc le plan.
     """
     p1,p2,p3 = threePointsCoords
-    print('P1 dans planEqution() : \n',p1)
+    """ print('P1 dans planEqution() : \n',p1)
     print('P2 dans planEqution() : \n',p2)
-    print('P3 dans planEqution() : \n',p3)
-    v1 = (p2-p1)/np.abs(np.max(np.abs(p2)-np.abs(p1)))
-    v2 = (p3-p1)/np.abs(np.max(np.abs(p3)-np.abs(p1)))
+    print('P3 dans planEqution() : \n',p3) """
+    v1 = (p2-p1)#/np.abs(np.max(np.abs(p2)-np.abs(p1)))
+    v2 = (p3-p1)#/np.abs(np.max(np.abs(p3)-np.abs(p1)))
     #print("v1,v2 : ",v1,v2)
     vn = np.cross(v1,v2)
     vn = vn/np.linalg.norm(vn)
@@ -120,11 +96,105 @@ def get_deltaVs(sommets,Q):
     #print('________________________')
     return deltaVs
 
+def init(sommets,faces):
+    #Calcul de Q pour tous les sommets initiaux
+    points_in_surface = get_Points_in_surface(sommets,faces)
+    Kps = get_Kps(sommets,points_in_surface)
+    #print('Kps : \n',Kps)
+    #Q = get_Q(Kps)
+    #deltaVs = get_deltaVs(sommets,Q)
+    #print(Q)
+    sommetsCLass = []
+    for i in range(len(sommets)):
+        sommetsCLass.append(Sommet(sommets[i]))
+        sommetsCLass[-1].set_Kp(Kps[i])
+        sommetsCLass[-1].set_Q()
+        sommetsCLass[-1].set_surfaces(points_in_surface[i])
+        g = sommetsCLass[-1]
+        c = np.concatenate((g.coords, np.array([1])),axis=0)
+        #print(c)
+        #print('cost : ',c@g.Q@c.transpose())
+    
+    #Selection des paires valides
+    validPairsIndex = get_validPairs(sommets,faces)
 
+    return validPairsIndex,sommetsCLass
 
 def cost(v,Q):
     vt = np.transpose(v)
     return (vt@Q@v)[0][0]
 
-def contraction():
-    return
+def contraction(sommets, faces): 
+    #prend la liste des sommets et la liste des faces en entrée
+    sommetsCoords = sommets
+    validPairsIndex, sommets = init(sommets ,faces)
+    print('SommetsCoords : \n',sommetsCoords)
+    #print('Valid pairs : \n',validPairsIndex)
+    """
+    A TROUVER : QUAND EST-CE QU'ON S'ARRETE?? --> il faut donner un nombre de faces à obtenir à l'avance
+    """
+
+    contractedSommets = []
+    newDv = []
+    newDic = []
+    for pair in validPairsIndex:
+        print('-------------')
+        print('Pair : ',pair)
+        print('Pair coords : \n',sommets[pair[0]].coords ,'\n--\n', sommets[pair[1]].coords)
+        print('Pair Qs : \n',sommets[pair[0]].Q ,'\n--\n', sommets[pair[1]].Q)
+        Q = sommets[pair[0]].Q + sommets[pair[1]].Q 
+        print('Q : \n',Q)
+        Qp = [[Q[0][0],Q[0][1],Q[0][2],Q[0][3]],
+            [Q[0][1],Q[1][1],Q[1][2],Q[1][3]],
+            [Q[0][2],Q[1][2],Q[2][2],Q[2][3]],
+            [0      ,0      ,0      ,1      ]]
+        Qp = np.array(Qp)
+        print('Qp : \n',Qp)
+        print('Det : \n',np.linalg.det(Qp))
+
+        if np.linalg.det(Qp) > 10**(-4):
+            Qp = np.linalg.inv(Qp)
+            v = Qp.dot(np.array([[0],[0],[0],[1]]))
+        else:
+            v= (sommets[pair[0]].coords + sommets[pair[1]].coords)/2
+        
+        contractedSommets.append(v[0:3])
+        #print('new V : \n',v)
+        Dv = cost(v,Q)
+        #print('Dv : \n',Dv)
+        newDv.append(Dv)
+        newDic.append((Dv,v,pair))
+        #print('----------')
+
+
+
+
+    print('--------------------------------')
+    contractedSommets = np.array(contractedSommets)
+    newDv = np.array(newDv)
+    print('NewDic : ')
+    for dic in newDic:
+        for truc in dic:
+            print(truc)
+        print('---')
+
+    newDic = sorted(newDic, key=lambda x: x[0]) #Trie selon le premier élément du tuple, ici le coût
+    #print('New dic : \n',newDic)
+
+    minCost, minV, minPair = newDic[0]
+
+
+    newSommets = copy.deepcopy(sommetsCoords)
+
+    print('sommets not updated : \n',newSommets)
+    newSommets[minPair[0]] = np.transpose(minV[0:3])
+    newSommets[minPair[1]] = np.transpose(minV[0:3])
+    #_,idx = np.unique(newSommets, axis=0, return_index=True)
+    #idx = np.sort(idx)
+    #newSommets = newSommets[idx]
+    print('sommets updated without doublons : \n',newSommets)
+    #print('Faces : \n',faces)
+
+    print('----------------FIN DE L"ITERATION----------------------')
+    plotScatterMatplot(newSommets)
+    return newSommets,faces
