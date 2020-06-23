11f49ab7aa403f9585ab9fbf3350dd712417af0a
Gaspard <47181975+GaspardLauras@users.noreply.github.com>
2020-06-24 00:59:14 +0200

    ajout ecriture dans fichier

--- MeshSimplification.py
+++ MeshSimplification.py
@@ -8,32 +8,39 @@ from Sommet import Sommet
 import copy
 
 
-offName = "OFF/sphere.off"
+offName = "sphere.off"
 
 #########################################
 #       Extraction des données:         #
 #########################################
-mesh = meshio.read(filename=offName,file_format="off")
+mesh = meshio.read(filename="OFF/"+offName,file_format="off")
 sommets = mesh.points
 sommetsInit = copy.deepcopy(sommets)
 faces = mesh.cells[0].data
 plotMesh(sommetsInit,faces,'AVANT')
+n_faces = len(faces)
+print(n_faces)
 
 """ 
 print('Sommets initiaux : \n',sommets)
 print('Faces initiales : \n',faces)
 print('------------------Début des itérations----------------------') """
 
-
+i=0
 ################################
-for i in range(20):
+while len(faces)>2:
+    i+=1
     sommets, faces = contraction(sommets, faces)
-    """ print('Sommets ',i,' : \n',sommets)
-    print('Faces ',i,' : \n',faces)
-    print('----------------FIN DE L"ITERATION #',i,'----------------------') """
+    print('Itération ',i,'\n    ',len(faces))
     
 ################
 
-
+print(len(faces))
 #plot2ScatterMatplot(sommetsInit,sommets)
 plotMesh(sommets,faces,'APRES')
+
+print(sommets)
+cells=[('triangle',faces)]
+
+result = meshio.Mesh(sommets,cells)
+meshio.write("OFF_results/result_"+offName,result)
